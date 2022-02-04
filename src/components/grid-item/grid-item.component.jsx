f6b7356c867ec9d64bc8a144e251519e82c1118d
crosgar <cecilia.rossi.garcia@gmail.com>
2022-02-04 15:23:45 -0800

    working on buttons for favoriting

--- src/components/grid-item/grid-item.component.jsx
+++ src/components/grid-item/grid-item.component.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./grid-item.styles.scss";
 import PictureModal from "../modal/modal.component";
+import Button from '@mui/material/Button';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const GridItem = ({ preview, largeImg, altText, link }) => {
    const [showModal, setShowModal] = useState(false);
@@ -9,10 +11,15 @@ const GridItem = ({ preview, largeImg, altText, link }) => {
       setShowModal(prevState => !prevState);
    }
 
+
    return (
       <div className="grid-item">
          <div className="img-container" onClick={toggleModal}>
             <img src={preview} alt={altText} className="img"></img>
+         </div>
+         <div className="btn-container">
+               {/* <Button color="secondary" className="fave-btn"><FavoriteIcon/></Button> */}
+               {/* <Button variant="outlined" color="secondary">Expand</Button> */}
          </div>
          
          <PictureModal showModal={showModal} toggleModal={toggleModal} imgSrc={largeImg} altText={altText} link={link}/>
