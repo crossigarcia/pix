3d67bf4d5d121ad77cb2fb75e88b14f39cdde8c8
crosgar <cecilia.rossi.garcia@gmail.com>
2022-02-04 14:39:23 -0800

    app.js switch to functional component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import Homepage from "./pages/homepage/homepage.component";
 import Home from "./pages/homepage/homepage.component";
 import './App.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {}
-  }
-  render () {
-    return (
-      <div className="App">
-        <Homepage/>
-        <Home />
-      </div>
-    );
-  }
-  
+const App = () => {
+  return (
+    <div className="App">
+      <Homepage/>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
