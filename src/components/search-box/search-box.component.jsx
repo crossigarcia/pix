25bbc02a407028213cc4c85748d140993ea27ef1
crosgar <cecilia.rossi.garcia@gmail.com>
2022-02-03 17:39:09 -0800

    styled search

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./search-box.styles.scss";
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
 const SearchBox = (props) => (
-   <div className="search-conatiner">
-      <input type="text" name="keyword" id="keyword" className="search-field" onChange={props.handleChange}></input>
-      <button type="submit" name="search-btn" id="search-btn" onClick={props.onClick}>SEARCH</button>
+   <div className="search-container">
+      <input type="text" name="keyword" id="keyword" className="search-field" onChange={props.handleChange} placeholder="Search"></input>
+      <button type="submit" name="search-btn" className="search-btn" onClick={props.onClick}>
+         <SearchRoundedIcon />
+      </button>
    </div>
 );
 
