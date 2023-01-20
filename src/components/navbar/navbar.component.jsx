fc586a9e4ce50f38870d422fafab162052d72dbe
crosgar <cecilia.rossi.garcia@gmail.com>
2023-01-20 10:23:05 -0800

    browser router issue fixed

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -4,7 +4,7 @@ import "./navbar.styles.scss";
 
 const Nav = () => (
    <div className="navbar">
-      <Link to="/" className="nav-link">Class Component</Link>
+      <Link to="/pix/" className="nav-link">Class Component</Link>
       <Link to="/functional" className="nav-link">Functional Component</Link>
       {/* <Link to="/favorites" className="nav-link">Favorites</Link> */}
    </div>
